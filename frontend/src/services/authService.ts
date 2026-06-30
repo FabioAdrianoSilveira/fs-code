8b9b53a241642584653a34f03f203bfa2a8808ed
Fábio Adriano Silveira <85971223+FabioAdrianoSilveira@users.noreply.github.com>
2026-06-30 03:36:37 -0300

    Delete frontend directory

--- frontend/src/services/authService.ts
+++
@@ -1,7 +0,0 @@
-export async function login(email: string, senha: string) {
-  const response = await fetch(
-    `http://localhost:8080/auth/login?email=${email}&senha=${senha}`,
-    { method: "POST" },
-  );
-  return await response.text();
-}
